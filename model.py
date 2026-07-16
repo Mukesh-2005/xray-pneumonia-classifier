448f69a664b0988c16d2cd0c92366c2005e4d226
Mukesh.k <starmukesh2005@gmail.com>
2026-07-16 18:41:15 +0530

    Update model.py

--- model.py
+++ model.py
@@ -55,6 +55,6 @@ if __name__ == "__main__":
     
     # Check if GPU available
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print(f"\n✅ Device: {device}")
+    print(f"\n Device: {device}")
     
     print("\n Model ready!")
